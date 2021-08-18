f12625a5566049bef3a2cfb5afa7244a6772dd68
Kai Sun <autosunkai@gmail.com>
2021-08-18 11:01:06 +0800

    🚀 RELEASE: Resume V1.0.

--- Copy&Paste.bat
+++ Copy&Paste.bat
@@ -2,7 +2,11 @@ chcp 65001
 
 @echo off 
 
-copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历.pdf"
+@REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-测试开发.pdf"
+@REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-服务端开发.pdf"
+@REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-后端开发.pdf"
+@REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-后台开发.pdf"
+copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-软件开发.pdf"
 
 copy "resume-zh.pdf" "C:\Users\Kai\OneDrive - CodingCatDev\Public\Resume\Resume.pdf"
 
