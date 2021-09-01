78f58b7685839d8692a9ea342209faf4f8fe50a6
Kai Sun <autosunkai@gmail.com>
2021-09-01 22:50:13 +0800

    👌 UPDATE: Remove database.

--- Copy&Paste.bat
+++ Copy&Paste.bat
@@ -3,10 +3,8 @@ chcp 65001
 @echo off 
 
 @REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-测试开发.pdf"
-@REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-服务端开发.pdf"
-@REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-后端开发.pdf"
 @REM copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-后台开发.pdf"
-copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-软件开发.pdf"
+copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历-后端开发.pdf"
 
 copy "resume-zh.pdf" "C:\Users\Kai\OneDrive - CodingCatDev\Public\Resume\Resume.pdf"
 
